4b421bf4e173f66244b1b0dab834e915b203fd37
Bhoomika <bhoomikahm18@gmail.com>
2023-12-27 11:58:53 +0530

    Function for buttons to increment, decrement and reset the counter value

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import "./App.css";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
   const counter = useSelector((state) => state.counter);
-  function increment(){}
-  function decrement(){}
+  const dispatch = useDispatch();
+
+  function increment(){
+    dispatch({type: 'INC'});
+  }
+  function decrement(){
+    dispatch({type: "DEC"});
+  }
+  function reset(){
+    dispatch({type: "RES"})
+  }
   return (
     <div>
       <h1>Counter App</h1>
       <h2>{counter}</h2>
       <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,19 @@
 import { createStore } from "redux";
 
-function reducerFun(state = { counter: 10 }, action) {
-    return state;
+function reducerFun(state = { counter: 0 }, action) {
+  //They should be synchronous function
+  //We should not mutate the original state
+
+  if (action.type === "INC") {
+    return { counter: state.counter + 1 };
+  }
+  if (action.type === "DEC") {
+    return { counter: state.counter - 1 };
+  }
+  if(action.type == "RES"){
+    return {counter: 0};
+  }
+  return state;
 }
 
 const store = createStore(reducerFun);
